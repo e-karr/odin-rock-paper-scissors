c269dcfdc78d374c7bc9ba210bde80e27cd70075
Elizabeth Karr <e.karr93@icloud.com>
2023-02-17 15:48:53 -0600

    add event listeners for button border transition. adds selected class when button clicked

--- script.js
+++ script.js
@@ -1,32 +1,53 @@
 let options = ["rock", "paper", "scissors"];
 
-    function getComputerChoice() {
-        let randomIndex = Math.floor(Math.random() * options.length)
-        let computerSelection = options[randomIndex]
-        return computerSelection;
+let rock = document.querySelector("#rock");
+let paper = document.querySelector("#paper");
+let scissors = document.querySelector("#scissors");
+let buttons = document.querySelectorAll(".player-button");
+
+rock.addEventListener("click", playerSelection);
+paper.addEventListener("click", playerSelection);
+scissors.addEventListener("click", playerSelection);
+
+buttons.forEach(button => button.addEventListener('transitionend', removeSelection));
+
+function playerSelection(e) {
+    this.classList.add('selected');
+}
+
+function removeSelection(e) {
+    this.classList.remove('selected');
+}
+
+
+
+function getComputerChoice() {
+    let randomIndex = Math.floor(Math.random() * options.length)
+    let computerSelection = options[randomIndex]
+    return computerSelection;
+}
+
+function getPlayerChoice() {
+    let playerSelection = prompt("Select rock, paper, or scissors. ");
+    
+    playerSelection = playerSelection.toLowerCase().trim();
+
+    while (!options.includes(playerSelection)) {
+        playerSelection = prompt("Invalid selection.\nSelect rock, paper, or scissors: ");
     }
 
-    function getPlayerChoice() {
-        let playerSelection = prompt("Select rock, paper, or scissors. ");
-        
-        playerSelection = playerSelection.toLowerCase().trim();
+    return playerSelection;
+}
 
-        while (!options.includes(playerSelection)) {
-            playerSelection = prompt("Invalid selection.\nSelect rock, paper, or scissors: ");
-        }
+function playSingleRound(playerSelection, computerSelection) {
 
-        return playerSelection;
+    if (playerSelection === computerSelection){
+        return "Tie!"
+    } else if ((playerSelection === "paper" && computerSelection === "scissors") || 
+                (playerSelection === "rock" && computerSelection === "paper") ||
+                (playerSelection === "scissors" && computerSelection === "rock")) {
+        return `You lose! ${computerSelection} beats ${playerSelection}.`;
+    } else {
+        return `You win! ${playerSelection} beats ${computerSelection}.`;
     }
-
-    function playSingleRound(playerSelection, computerSelection) {
-
-        if (playerSelection === computerSelection){
-            return "Tie!"
-        } else if ((playerSelection === "paper" && computerSelection === "scissors") || 
-                    (playerSelection === "rock" && computerSelection === "paper") ||
-                    (playerSelection === "scissors" && computerSelection === "rock")) {
-            return `You lose! ${computerSelection} beats ${playerSelection}.`;
-        } else {
-            return `You win! ${playerSelection} beats ${computerSelection}.`;
-        }
-    }
+}
